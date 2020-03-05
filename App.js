104ec4b56425ad0748660afff837ccb04903cd6e
Madylune <nginn.julie@gmail.com>
2020-03-05 15:53:45 +0100

    feat(Topbar): Adding Topbar

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import Topbar from './components/Topbar'
+
+export default function App() {
+  return (
+    <View>
+      <Topbar />
+    </View>
+  )
+}
---
+++ components/Topbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Header, Icon } from 'react-native-elements'
+import styled from 'styled-components'
+
+const StyledRightComponent = styled(View)`
+  display: flex;
+  flex-direction: row;
+`
+
+const StyledIcon = styled(Icon)`
+  margin: 10px;
+`
+
+const RightComponent = () => (
+  <StyledRightComponent>
+    <StyledIcon name='bell' type='font-awesome' color='#fff' containerStyle={{ marginRight: 5, marginLeft: 5 }} />
+    <StyledIcon name='user-circle' type='font-awesome' color='#fff' containerStyle={{ marginRight: 5, marginLeft: 5 }} />
+  </StyledRightComponent>
+)
+
+const Topbar = () => 
+  <Header
+    leftComponent={{ icon: 'search', color: '#fff' }}
+    centerComponent={{ text: 'WORLD', style: { color: '#fff', fontWeight: '800' } }}
+    rightComponent={<RightComponent />}
+    containerStyle={{
+    backgroundColor: '#0B3B2E',
+    justifyContent: 'space-around',
+    }}
+  />
+
+export default Topbar
